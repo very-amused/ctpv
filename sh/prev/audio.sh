de72ab391aec652197e2ef381cacf0476a5dd5fe
very-amused <very-amused@pm.me>
2024-06-11 09:36:49 -0400

    Fix audio thumbnails

--- sh/prev/audio.sh
+++ sh/prev/audio.sh
@@ -4,10 +4,10 @@ audio() {
 	ffmpegthumbnailer -i "$f" -s 0 -q 50% -t 10 -o "$cache_f" 2>/dev/null
 }
 
-if exists ffmpeg; then
+if exists ffmpegthumbnailer; then
+	convert_and_show_image audio
+elif exists ffmpeg; then
 	x="$(ffmpeg -hide_banner -i "$f" 2>&1)"
 	printf '%s\n' "$x"
 	y=$((y + $(printf '%s\n' "$x" | wc -l)))
 fi
-
-convert_and_show_image audio
